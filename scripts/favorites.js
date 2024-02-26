8487fe4005646e5d8f35676cc6f67174fd92b3f3
Felipe Leão <felipeleao.tech@gmail.com>
2024-02-26 16:02:08 -0300

    layout finalizado

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,15 +1,11 @@
 export default function favoriteProfile() {
-  
-
   // form.addEventListener("submit", (event) => {
   //   event.preventDefault();
-
   //   textValue = formInput.value;
   //   formInput.value = "";
   //   console.log(textValue);
   //   requisicaoAPI();
   // });
-
   // function requisicaoAPI() {
   //   const api = fetch(`https://api.github.com/users/${textValue}`);
   //   api
@@ -20,3 +16,24 @@ export default function favoriteProfile() {
   //     });
   // }
 }
+
+// rascunho html item
+
+/* <tr class="table-favorites-body-line">
+                <td class="table-favorites-body-profile">
+                  <a href="" target="_blank">
+                    <img
+                      src="https://avatars.githubusercontent.com/u/87910555?v=4"
+                      alt=""
+                    />
+                    Felipe
+                  </a>
+                </td>
+                <td>9</td>
+                <td>32</td>
+                <td class="table-favorites-body-button-remove">
+                  <button class="table-favorites-body-remove">
+                    <img src="assets/trash-solid.svg" alt="" />
+                  </button>
+                </td>
+              </tr> */
