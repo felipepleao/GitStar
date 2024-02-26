34b618c484b1f022f08e965fe3f179b2f03fe262
Felipe Leão <felipeleao.tech@gmail.com>
2024-02-26 16:30:37 -0300

    layout finalizado

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,20 +1,20 @@
 export default function favoriteProfile() {
-  // form.addEventListener("submit", (event) => {
-  //   event.preventDefault();
-  //   textValue = formInput.value;
-  //   formInput.value = "";
-  //   console.log(textValue);
-  //   requisicaoAPI();
-  // });
-  // function requisicaoAPI() {
-  //   const api = fetch(`https://api.github.com/users/${textValue}`);
-  //   api
-  //     .then((res) => res.json())
-  //     .then((res) => {
-  //       nome.textContent = res.login;
-  //       repositorio.textContent = res.public_repos;
-  //     });
-  // }
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    textValue = formInput.value;
+    formInput.value = "";
+    console.log(textValue);
+    requisicaoAPI();
+  });
+  function requisicaoAPI() {
+    const api = fetch(`https://api.github.com/users/${textValue}`);
+    api
+      .then((res) => res.json())
+      .then((res) => {
+        nome.textContent = res.login;
+        repositorio.textContent = res.public_repos;
+      });
+  }
 }
 
 // rascunho html item
